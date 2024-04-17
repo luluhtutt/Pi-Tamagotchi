40247432fd656883f3a58de4b0de283996ed0e00
Lulu Htutt <lh543@cornell.edu>
2024-04-17 19:23:23 -0400

    added relational db

--- init_db.py
+++ init_db.py
@@ -9,31 +9,38 @@ cursor.execute("DROP TABLE IF EXISTS Tamagotchi")
 
 tamagotchi_table = """ CREATE TABLE Tamagotchi (
     Name varchar(255),
-    Id int NOT NULL PRIMARY KEY,
-    Age float,
-    Image varchar(255),
-    Health int,
-    Hunger int,
-    Happiness int
+    TID int NOT NULL PRIMARY KEY,
+    Image varchar(255)
 ); """
 cursor.execute(tamagotchi_table)
 
-images = ["fox.png", "sanaart.png", "zebra.png"]
-
-for i in range(10):
-    filename = images[random.randint(0, len(images)-1)]
-    add_tamagotchi =  """ INSERT INTO Tamagotchi VALUES ('Test', """ + str(i) + """, 0.0, '""" + str(filename) + """', 100, 100, 100)"""
-    cursor.execute(add_tamagotchi)
-
-user_table = """ CREATE TABLE Tamagotchi (
+user_table = """ CREATE TABLE User (
     Name varchar(255),
-    Id int NOT NULL PRIMARY KEY,
+    UID int NOT NULL PRIMARY KEY,
+    Image varchar(255)
+); """
+cursor.execute(user_table)
+
+relation_table = """ CREATE TABLE Relation (
+    UID int NOT NULL PRIMARY KEY,
+    TID int NOT NULL PRIMARY KEY,
     Age float,
-    Image varchar(255),
     Health int,
     Hunger int,
     Happiness int
 ); """
+cursor.execute(relation_table)
+
+# create tamagotchis
+images = ["fox.png", "sanaart.png", "zebra.png"]
+names = ["Bob", "Pacmen", "Jaredz"]
+
+for i in range(10):
+    n = random.randint(0, len(images)-1)
+    filename = images[n]
+    name = names[n]
+    add_tamagotchi =  """ INSERT INTO Tamagotchi VALUES ('""" + name + """', """ + str(i) + """, '""" + str(filename) + """')"""
+    cursor.execute(add_tamagotchi)
 
 connection.commit()
 
