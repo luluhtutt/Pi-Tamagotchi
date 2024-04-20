031421b76f5fb6fbfa01540dbcdceec711fdea37
luluhtutt <luluhtutt@yahoo.com>
2024-04-20 12:53:30 -0400

    Trying and testing

--- init_db.py
+++ init_db.py
@@ -6,35 +6,51 @@ connection = sqlite3.connect("tamagotchi.db")
 cursor = connection.cursor()
 
 cursor.execute("DROP TABLE IF EXISTS Tamagotchi")
+cursor.execute("DROP TABLE IF EXISTS User")
+cursor.execute("DROP TABLE IF EXISTS Relation")
 
 tamagotchi_table = """ CREATE TABLE Tamagotchi (
     Name varchar(255),
-    Id int NOT NULL PRIMARY KEY,
-    Age float,
-    Image varchar(255),
-    Health int,
-    Hunger int,
-    Happiness int
+    TID int NOT NULL PRIMARY KEY,
+    Tim varchar(255)
 ); """
 cursor.execute(tamagotchi_table)
 
-images = ["fox.png", "sanaart.png", "zebra.png"]
-
-for i in range(10):
-    filename = images[random.randint(0, len(images)-1)]
-    add_tamagotchi =  """ INSERT INTO Tamagotchi VALUES ('Test', """ + str(i) + """, 0.0, '""" + str(filename) + """', 100, 100, 100)"""
-    cursor.execute(add_tamagotchi)
-
-user_table = """ CREATE TABLE Tamagotchi (
+user_table = """ CREATE TABLE User (
     Name varchar(255),
-    Id int NOT NULL PRIMARY KEY,
-    Age float,
-    Image varchar(255),
-    Health int,
-    Hunger int,
-    Happiness int
+    UID int NOT NULL PRIMARY KEY,
+    Uim varchar(255)
 ); """
 
-connection.commit()
+cursor.execute(user_table)
 
+relation_table = """ CREATE TABLE Relation (
+    UID int NOT NULL,
+    TID int NOT NULL,
+    Age float,
+    Health int,
+    Hunger int,
+    Happiness int,
+    PRIMARY KEY (UID, TID)
+); """
+cursor.execute(relation_table)
+
+# create tamagotchis
+images = ["fox.png", "sanaart.png", "zebra.png"]
+names = ["Bob", "Pacmen", "Jaredz"]
+
+for i in range(10):
+    n = random.randint(0, len(images)-1)
+    filename = images[n]
+    name = names[n]
+    add_tamagotchi =  """ INSERT INTO Tamagotchi VALUES ('""" + name + """', """ + str(i) + """, '""" + str(filename) + """')"""
+    cursor.execute(add_tamagotchi)
+
+add_user =  """ INSERT INTO User VALUES ('Lulu', 1, 'lulu.png');"""
+add_relation = """ INSERT INTO Relation VALUES (1, 1, 0.5, 90, 80, 70);"""
+cursor.execute(add_user)
+cursor.execute(add_relation)
+
+
+connection.commit()
 connection.close()
