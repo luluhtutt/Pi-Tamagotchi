10f429cfbb2c930771026720a1d0bb9263c08cf0
luluhtutt <luluhtutt@yahoo.com>
2024-04-29 16:17:54 -0400

    started file screen

--- game.py
+++ game.py
@@ -42,20 +42,22 @@ pygame.display.update()
 
 # font settings
 font_main_buttons = pygame.font.Font(None, 25)
-font_menu_buttons = pygame.font.Font(None, 18)
+font_action_buttons = pygame.font.Font(None, 18)
 font_time = pygame.font.Font(None, 30)
+font_files = pygame.font.Font(None, 20)
 
 # displaying the tamagotchi
-images_select = """ select Tim from Tamagotchi where TID = """ + str(TID) + """ limit 1;"""
-cursor.execute(images_select)
+def choose_tama(TID):
+    images_select = """ select Tim from Tamagotchi where TID = """ + str(TID) + """ limit 1;"""
+    cursor.execute(images_select)
 
-image_path = "images/" + cursor.fetchone()[0]
-tama_im = pygame.image.load(image_path)
-tama_im = pygame.transform.scale(tama_im, (100, 100))
-tama_rect = tama_im.get_rect()
-tama_rect.x = 110
-tama_rect.y = 70
-
+    image_path = "images/" + cursor.fetchone()[0]
+    tama_im = pygame.image.load(image_path)
+    tama_im = pygame.transform.scale(tama_im, (100, 100))
+    tama_icon = pygame.transform.scale(tama_im, (35, 35))
+    tama_rect = tama_im.get_rect()
+    tama_rect.x = 110
+    tama_rect.y = 70
 
 # displaying status bars
 
@@ -96,19 +98,37 @@ def status_bars():
 
     pygame.display.update()
 
+def files():
+    file_pos = [(20, 10), (20, 58), (20, 106), (20, 154), (20, 202), (180, 10), (180, 58), (180, 106), (180, 154), (180, 202)]
+    cursor.execute("""SELECT * FROM Relation WHERE UID=""" + str(UID))
+    cur_tamas = cursor.fetchall()
+    cur_tamas.extend([None] * (10 - len(cur_tamas)))
+    # print("cur tamas: ", cur_tamas)
+    for t in range(len(cur_tamas)):
+        if(cur_tamas[t] is None):
+            text_surface = font_files.render('EMPTY', True, WHITE)
+            rect = text_surface.get_rect(topleft=file_pos[t]) 
+            lcd.blit(text_surface, rect)
+        else:
+            text_surface = font_files.render('TID: ' + str(cur_tamas[t][1]), True, WHITE)
+            rect = text_surface.get_rect(topleft=file_pos[t]) 
+            lcd.blit(text_surface, rect)
+        pygame.display.update()
+
 times = {0: "12:00 AM", 15: "1:00 AM", 30: "2:00 AM", 45: "3:00 AM", 60: "4:00 AM", 75: "5:00 AM", 90: "6:00 AM", 105: "7:00 AM",
 120: "8:00 AM", 135: "9:00 AM", 150: "10:00 AM", 165: "11:00 AM", 180: "12:00 PM", 195: "1:00 PM", 210: "2:00 PM", 225: "3:00 PM", 240: "4:00 PM", 
 255: "5:00 PM", 270: "6:00 PM", 285: "7:00 PM", 300: "8:00 PM", 315: "9:00 PM", 330: "10:00 PM", 345: "11:00 PM"}
 
 user_login_buttons = {'New Player': (160, 60), 'Returning Player': (160, 120), 'Quit': (160, 180)}
 main_buttons = {'Menu': (20, 20), 'Quit':(20, 60)}
-menu_buttons = {'Info': (20, 20), 'Feed': (20,50), 'Sleep': (20, 80), 'Clean': (20, 110), 'Back': (20,140), 'Quit': (20,170)}
+action_buttons = {'Info': (20, 20), 'Feed': (20,50), 'Sleep': (20, 80), 'Clean': (20, 110), 'Back': (20,140), 'Quit': (20,170)}
 
 # state variables
 time_counter = 0
 playing = True
 current_screen = 'login' # current_screen is either 'login', 'file', or 'main'
-current_menu = 'main' # current_menu is either 'main', 'actions', or 'info'
+current_menu = 'main' # current_menu is either 'main', 'files', 'actions', or 'info'
+selection_max = {'main': 2, 'files': 10, 'actions': 6}
 selection = 0 # selection is the index of the menu item that is currently selected
 
 def GPIO17_callback(channel):
@@ -156,17 +176,20 @@ while(playing):
                     user_name = "User" + str(UID)
                     cursor.execute("""INSERT INTO User VALUES ('""" + user_name + """' ,""" + str(UID) + """, '""" + fp + """');""")
                     capture_image(fp)
+                    current_screen = 'file'
                     
                 elif(y > 90 and y < 150):
                     print("returning player")
                     UID = classify_image(cursor)
-                    print(UID)
+                    current_screen = 'file'
+                    # print(UID)
 
                 else:
                     print("quit pressed")
                     playing = False
                 
-
+    elif(current_screen == 'file'):
+        files()
     # main screen
     elif(current_screen == 'main'):
         # printing time
@@ -204,7 +227,7 @@ while(playing):
         elif(current_menu == 'actions'):
             pygame.draw.rect(lcd, (0,0,0), pygame.Rect(0, 0, 120, 200))
             pygame.display.update()
-            for k,v in menu_buttons.items():
+            for k,v in action_buttons_buttons.items():
                 # pygame.draw.rect(lcd, (0,0,255), pygame.Rect(0, v[0]-20, 120, 40))
                 text_surface = font_main_buttons.render('%s'%k, True, WHITE)
                 rect = text_surface.get_rect(topleft=v) 
@@ -242,114 +265,6 @@ while(playing):
                 #         if(x < )
         time_counter += 1
     time.sleep(1)
-
-# print("hungry: ", cursor.fetchone()[0])
-
-# touch_buttons = {'Start': (80, 180), 'Quit':(240, 180)}
-
-# play_buttons = {'Pause': (40, 200), 'Fast': (120, 200), 'Slow': (200, 200), 'Back': (280, 200)}
-
-# screen_taps = [] 
-
-# pygame.display.update() 
-
-# time_end = time.time() + 60
-
-# sleep_time = 0.01
-
-# can_start = False
-# code_quit = True
-# pause_game = False
-
-# while (code_quit and time.time() < time_end): 
-#     if ( not GPIO.input(17)):
-#         break
-
-#     pitft.update()
-
-#     if(can_start):
-#         if(not pause_game):
-#             christmas_ballrect = christmas_ballrect.move(speed)
-#             bballrect = bballrect.move(b_speed)
-#             if bballrect.colliderect(christmas_ballrect):
-#                 speed[0] = -speed[1]
-#                 speed[1] = -speed[0]
-#                 b_speed[0] = -b_speed[1] 
-#                 b_speed[1] = -b_speed[0]
-#             if christmas_ballrect.left < 0 or christmas_ballrect.right > width:
-#                 speed[0] = -speed[0]
-#             if christmas_ballrect.top < 0 or christmas_ballrect.bottom > height:
-#                 speed[1] = -speed[1]
-#             if bballrect.left < 0 or bballrect.right > width:
-#                 b_speed[0] = -b_speed[0]
-#             if bballrect.top < 0 or bballrect.bottom > height:
-#                 b_speed[1] = -b_speed[1]
-
-#         lcd.fill(black)
-#         lcd.blit(christmas_ball, christmas_ballrect)
-#         lcd.blit(bball, bballrect)
-
-#         for k,v in play_buttons.items(): 
-#             text_surface = font_small.render('%s'%k, True, WHITE)
-#             rect = text_surface.get_rect(center=v) 
-#             lcd.blit(text_surface, rect)
-
-#     else:
-#         for k,v in touch_buttons.items(): 
-#             text_surface = font_big.render('%s'%k, True, WHITE)
-#             rect = text_surface.get_rect(center=v) 
-#             lcd.blit(text_surface, rect) 
-
-#     pygame.display.update()
-    
-#     for event in pygame.event.get(): 
-#         if(event.type is MOUSEBUTTONDOWN): 
-#             x,y = pygame.mouse.get_pos()  
-#             if y < 120: 
-#                 screen_taps.append((x,y))
-#                 pygame.draw.rect(lcd, (0,0,0), pygame.Rect(60, 40, 150, 50))
-#                 pygame.display.update()
-#                 k = 'touch at ' + str(screen_taps[-1])
-#                 text_surface_2 = font_small.render('%s'%k, True, WHITE)
-#                 rect_2 = text_surface_2.get_rect(center=(120, 60)) 
-#                 lcd.blit(text_surface_2, rect_2)
-#                 pygame.display.update() 
-#                 print("button down")
-#                 print(x, y)
-#         elif (event.type is MOUSEBUTTONUP): 
-#             x,y = pygame.mouse.get_pos()
-#             if(not can_start):
-#                 if y >= 120 and x < 100: 
-#                     can_start = True
-#                 elif y >= 120 and x > 200:
-#                     print("quit pressed")
-#                     can_start = False
-#                     code_quit = False
-#                     break
-#             else:
-#                 if(y >= 180):
-#                     if(x < 80):
-#                         pause_game = not pause_game
-#                     elif(x < 160):
-#                         og_sleep_time = sleep_time
-#                         sleep_time /= 2
-#                         if sleep_time <= 0.00125: 
-#                             sleep_time = og_sleep_time
-#                     elif(x < 240):
-#                         og_sleep_time = sleep_time
-#                         sleep_time += 0.01
-#                         if sleep_time >= 0.05: 
-#                             sleep_time = 0.01
-#                     else:
-#                         lcd.fill(black)
-#                         can_start = False
-
-#     time.sleep(sleep_time)
-
-# if ( not GPIO.input(17)):
-#     pygame.quit() 
-#     import sys
-#     sys.exit(0)
 
 connection.commit()
 connection.close()
